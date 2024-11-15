90b53e7e047a0fb89adfbb1e71efcd29e7571a6c
astermangabat25 <157344930+astermangabat25@users.noreply.github.com>
2024-11-15 22:07:18 +0800

    Updated User

--- ClassScheduleMakerUser/src/app/entities/User.java
+++ ClassScheduleMakerUser/src/app/entities/User.java
@@ -1,10 +1,27 @@
 package app.entities;
 
+<<<<<<< Updated upstream
+=======
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+>>>>>>> Stashed changes
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+<<<<<<< Updated upstream
+=======
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+>>>>>>> Stashed changes
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -25,6 +42,7 @@ public class User {
 	@Column
 	@NotNull
 	private String phoneNumber;
+<<<<<<< Updated upstream
 
 	public Long getStudentID() {
 		return studentID;
@@ -33,6 +51,12 @@ public class User {
 	public void setStudentID(Long studentID) {
 		this.studentID = studentID;
 	}
+=======
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "sectionPk", nullable = true)
+    private Set<Section> sections = new HashSet<>();
+>>>>>>> Stashed changes
 
 	public String getName() {
 		return name;
@@ -58,6 +82,25 @@ public class User {
 		this.phoneNumber = phoneNumber;
 	}
 
+<<<<<<< Updated upstream
+=======
+	public Long getUserPk() {
+		return userPk;
+	}
+
+	public void setUserPk(Long userPk) {
+		this.userPk = userPk;
+	}
+
+	public Set<Section> getSections() {
+		return sections;
+	}
+
+	public void setSections(Set<Section> sections) {
+		this.sections = sections;
+	}
+
+>>>>>>> Stashed changes
 	@Override
 	public String toString() {
 		return "User [studentID=" + studentID + ", name=" + name + ", course=" + course + ", phoneNumber=" + phoneNumber
